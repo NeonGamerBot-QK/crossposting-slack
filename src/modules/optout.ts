123f1730476412a84bf4bd57732183c0678143f0
Saahil <neon@saahild.com>
2024-11-26 23:16:54 -0500

    fix: make messages invs

--- src/modules/optout.ts
+++ src/modules/optout.ts
@@ -7,29 +7,49 @@ export default function (app: App, db: QuickDB) {
   app.command("/crossposting-optout", async (par) => {
     await par.ack();
     await db.set(`userdata_${par.body.user_id}.optout`, true);
-    await par.say(
-      "You have opted out of crossposting. You will no longer be able to use me. to opt back in please dm <@U07L45W79E1> or delete your data with /crossposting-deletemydata",
+    await app.client.chat.postEphemeral(
+      {
+        text: "You have opted out of crossposting. You will no longer be able to use me. to opt back in please dm <@U07L45W79E1> or delete your data with /crossposting-deletemydata",
+        channel: par.body.channel_id,
+        user: par.body.user_id
+      },
     );
   });
   app.command("/crossposting-forget-i-exist", async (par) => {
     await par.ack();
     await db.delete("userdata_" + par.body.user_id);
     await db.set(`userdata_${par.body.user_id}.optout`, true);
-    await par.say("Done, Now who are you again??");
+    // await par.say();
+    await app.client.chat.postEphemeral(
+      {
+        text: "Done, Now who are you again??",
+        channel: par.body.channel_id,
+        user: par.body.user_id
+      },
+    );
   });
 
   app.command("/crossposting-deletemydata", async (par) => {
     await par.ack();
     await db.delete("userdata_" + par.body.user_id);
-    par.say(
-      "Deleting your data...\n> note: this deletes all refrences of your user id. Including if you have been opted out.",
+
+    await app.client.chat.postEphemeral(
+      {
+        text: "Deleting your data...\n> note: this deletes all refrences of your user id. Including if you have been opted out.",
+        channel: par.body.channel_id,
+        user: par.body.user_id
+      },
     );
   });
   app.command("/crossposting-block-my-channel", async (par) => {
     await par.ack();
     if (!overrides.includes(par.body.user_id)) {
-      await par.say(
-        "You are not the channel manager for this channel.  OR fire dept",
+      await app.client.chat.postEphemeral(
+        {
+          text:         "You are not the channel manager (not implemented yet) for this channel.  OR fire dept",
+          channel: par.body.channel_id,
+          user: par.body.user_id
+        },
       );
       return;
     }
